7c999458346dcc3a729b670be6ab300ae7fafe4e
ChaimaaLachhab <chaimaalachhab555@gmail.com>
2024-09-18 18:14:17 +0100

    some modification in the components

--- artisanat-frontend/src/app/app.component.ts
+++ artisanat-frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {HeaderComponent} from "./shared/components/header/header.component";
+import {FooterComponent} from "./shared/components/footer/footer.component";
+import {HeroHomeComponent} from "./shared/components/hero-home/hero-home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeroHomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
