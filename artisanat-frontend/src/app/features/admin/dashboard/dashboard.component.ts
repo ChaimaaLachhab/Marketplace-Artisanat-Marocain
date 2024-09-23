99ca246ad0ba27cf2b58c1db4170362b0458f06b
ChaimaaLachhab <chaimaalachhab555@gmail.com>
2024-09-23 07:07:45 +0100

    add other component(checkout)

--- artisanat-frontend/src/app/features/admin/dashboard/dashboard.component.ts
+++ artisanat-frontend/src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {HeaderDashComponent} from "../../../shared/components/header-dash/header-dash.component";
+import {SidebarComponent} from "../../../shared/components/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderDashComponent,
+    SidebarComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
